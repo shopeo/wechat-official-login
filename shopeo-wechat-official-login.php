beec8204c08a3d55c6b3f477741d26352f2af410
jabber <2898117012@qq.com>
2023-04-17 12:00:08 +0800

    init

---
+++ shopeo-wechat-official-login.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: SHOPEO WeChat Official Login
+ * Plugin URI: https://wordpress.org/plugins/shopeo-wechat-official-login
+ * Description: WeChat Official Login
+ * Author: SHOPEO
+ * Version: 0.0.3
+ * Author URI: https://shopeo.cn
+ * License: GPL2+
+ * Text Domain: wechat-official-login
+ * Domain Path: /languages
+ * Requires at least: 5.9
+ * Requires PHP: 5.6
+ */
